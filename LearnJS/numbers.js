a8308166cde3a575e8cf0e4f80801bf16d660d4d
“tia-qa” <suchita-qa@outlook.com>
2026-02-27 22:16:55 -0500

    numbers

---
+++ LearnJS/numbers.js
@@ -0,0 +1,39 @@
+x = 6.011
+//round to nearest 
+y = Math.round(x) //7
+//round to lower number
+Math.floor(x) //6
+//round to higher number
+ceil = Math.ceil(x) //7
+console.log(ceil)
+
+console.log(y)
+
+maxnum = Math.max(1,2,3,43,5,633,733,833,9)
+console.log(maxnum)
+
+minnum = Math.min(1,2,3,43,5,633,733,833,9)
+console.log(minnum)
+
+x = 45.0000223
+y = Math.trunc(x) //45
+console.log(y)
+
+z = Math.pow(2,9) //8
+console.log(z)
+Math.sqrt(16) //4
+
+a = 2**6 //64
+console.log(a)
+
+randomnum = Math.random() //0 to 1
+console.log(randomnum)
+
+//w3 schools - JS MATH
+console.log(Math.PI)
+
+console.log(Math.sqrt(81))
+
+//==================================
+
+//moment js - date and time
